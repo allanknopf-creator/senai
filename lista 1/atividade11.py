81ca18b0a5083f941d2daf007ac3c76346811cc7
allanknopf-creator <allan_knopf@estudante.sesisenai.org.br>
2026-04-24 18:06:57 +0000

    update

--- lista 1/atividade11.py
+++ lista 1/atividade11.py
@@ -8,3 +8,13 @@ dias_restantes = resto % 30
 
 print("Tempo sem acidentes:")
 print(anos, "ano(s),", meses, "mes(es) e", dias_restantes, "dia(s)")
+
+ #"dias_trabalhados = int(input("Digite o número de dias trabalhados: "))
+
+# Cálculo de anos, meses e dias
+#anos = dias_trabalhados // 365
+#meses = (dias_trabalhados % 365) // 30
+#dias = (dias_trabalhados % 365) % 30
+
+# Exibição do resultado
+#print(f"{dias_trabalhados} dias equivalem a {anos} anos, {meses} meses e {dias} dias.")"
--- lista 1/atividade12.py
+++ lista 1/atividade12.py
@@ -1,10 +1,10 @@
 salario_inicial = float(input("Digite o salário inicial: R$ "))
 
 # aumento de 15%
-salario_com_aumento = salario_inicial * 1.15
+salario_com_aumento = salario_inicial + (salario_inicial * 0.15)
 
 # desconto de 8% de imposto
-salario_final = salario_com_aumento * 0.92
+salario_final = salario_com_aumento - (salario_com_aumento * 0.08)
 
 print("\nSalário inicial: R$ {:.2f}".format(salario_inicial))
 print("Salário com aumento: R$ {:.2f}".format(salario_com_aumento))
